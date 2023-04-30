3e914a9897443a3968730b6c1483d6ecb098991e
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2023-04-30 18:53:12 +1200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route(route)
+@app.route("/")
 def hello_world():
-    return render_template("index.html", title=route)
+    return render_template("index.html", title="Hello")
